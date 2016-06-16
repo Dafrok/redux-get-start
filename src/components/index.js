acc813fb6b36a22e322f3ba9a8ba3ab4ff81164e
Dafrok <o.o@mug.dog>
2016-06-16 19:22:09 +0800

    with dev tools

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import loggerMiddleware from 'redux-logger'
 import { Provider } from 'react-redux'
 import todoApp from '../reducers/index.js'
 import App from './app.js'
@@ -8,6 +10,9 @@ import App from './app.js'
 const root = document.createElement('section')
 document.body.appendChild(root)
 
-let store = createStore(todoApp)
+let store = createStore(todoApp, {}, compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, root)
